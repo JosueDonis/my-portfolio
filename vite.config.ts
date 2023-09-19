4e6fb3280e75e384ff99d0540fa5a02e40b98933
Josh <brayan120agustin@gmail.com>
2023-09-18 21:22:41 -0600

    Fixed fonts 👎

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import{ FontaineTransform }from"fontaine";
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(),  
+		FontaineTransform.vite({ fallbacks:["Poppins","Montserrat"]})],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
